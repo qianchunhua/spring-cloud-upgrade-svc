7ce2d1066ebddd11f4b76e70bd77a47a5c9b99f3
spring <myqianch@gmail.com>
2020-03-07 23:16:20 +0800

    创建支付模块

---
+++ pay-cloud-payment-svc/src/test/java/cn/itspring/cloud/PayCloudPaymentSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.itspring.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayCloudPaymentSvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
